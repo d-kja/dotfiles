b537fa0be6ff0229031fa2af8663ca3f8bd36b4d
d-kja <89911360+d-kja@users.noreply.github.com>
2025-12-14 05:39:32 -0300

    zbQn3OemWAIKNg+68LqAXuRjWObsk/Vc+bVVRD34aUA=

--- .config/hypr/scripts/reset.sh
+++ .config/hypr/scripts/reset.sh
@@ -2,6 +2,6 @@
 
 $HOME/.config/hypr/scripts/reset-bar.sh &
 $HOME/.config/hypr/scripts/reset-search.sh &
-# $HOME/.config/hypr/scripts/reset-background.sh &
+$HOME/.config/hypr/scripts/reset-background.sh &
 $HOME/.config/hypr/scripts/reset-spotify.sh &
 
